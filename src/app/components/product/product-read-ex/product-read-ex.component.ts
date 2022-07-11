be85ab4e7e9e5e65fb48fb8ef074adc893c604df
jwcamelo <jwcmade@gmail.com>
2022-07-11 12:18:49 -0300

    add read products table

---
+++ src/app/components/product/product-read-ex/product-read-ex.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { ProductReadExDataSource, ProductReadExItem } from './product-read-ex-datasource';
+
+@Component({
+  selector: 'app-product-read-ex',
+  templateUrl: './product-read-ex.component.html',
+  styleUrls: ['./product-read-ex.component.scss']
+})
+export class ProductReadExComponent implements AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ProductReadExItem>;
+  dataSource: ProductReadExDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  constructor() {
+    this.dataSource = new ProductReadExDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
